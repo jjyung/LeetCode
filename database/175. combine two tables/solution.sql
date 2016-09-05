2822d22341a5725bdd78065a8a8d272dbb3b3b3e
jjyung <jjyung1234@gmail.com>
2016-09-05 21:16:39 +0800

    Delete solution.sql

--- database/175. combine two tables/solution.sql
+++
@@ -1,8 +0,0 @@
-(select p1.FirstName, p1.LastName, a1.City, a1.State
-from Person p1, Address a1
-where p1.PersonId = a1.PersonId)
-union
-(select p2.FirstName, p2.LastName, null as City, null as State
-from Person p2
-where p2.PersonId not in
-  (select a2.PersonId from Address a2))
