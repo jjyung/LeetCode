a14dde97ab97d3eacbdfdf3053b6b1dedb8d7c7d
jjyung <jjyung1234@gmail.com>
2018-11-15 12:33:40 +0800

    Update solution.sql

--- database/easy/196. delete duplicate emails/solution.sql
+++ database/easy/196. delete duplicate emails/solution.sql
@@ -1,4 +1,14 @@
+# v1 Runtime: 812 ms
 delete p2
 from Person p1, Person p2
 where p1.Email = p2.Email
 and p1.Id < p2.Id;
+
+# v2 Runtime: 551 ms
+DELETE p
+FROM Person p, 
+    (SELECT MIN(Id) as Id, Email
+    FROM Person
+    GROUP BY Email) as keep
+WHERE p.Email = keep.Email
+AND p.Id != keep.Id;
